617adcafeff04e03bc2d77fe19aae03d5a0d64ee
Yvette Solorzano <solorzano.yvette@gmail.com>
2018-05-08 22:12:00 -0800

    Your Message

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # meridixstatic
+
+Still working: 
+- Carousel on tablet
+- Show/Hide mobile nav
+- Filters are hidden, trying to work on toggle.
---
+++ js/main.js
@@ -0,0 +1,78 @@
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+  
+  
+  
+function openTab(evt, tabName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+     tablinks[i].className = tablinks[i].className.replace(" selected", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " selected";
+}
+
+
+function myToggle() {
+    var toggle = document.getElementById("filterToggle");
+    if (toggle.style.display === "none") {
+        toggle.style.display = "block";
+    } else {
+        toggle.style.display = "none";
+    }
+}
+
+
+
+/*******************
+  menu toggle
+  ******************/
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+		
